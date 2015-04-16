8ea280906f214863a2fa1ae643b28cd3eb12fcaa
adammartin <ebondark.od@gmail.com>
2015-04-16 07:07:36 -0500

    Adding client type to see if that helps with authorization

--- lib/database.rb
+++ lib/database.rb
@@ -13,16 +13,27 @@ module Azure
       end
 
       def list
-        header = header Time.now.httpdate
-        JSON.parse(rest_client.get "#{context.endpoint}/#{resource_type}", header)
+        header = header Time.now.httpdate, "accept"
+        JSON.parse(rest_client.get url, header)
+      end
+
+      def create database_name
+        body = { "id" => database_name }
+        header = header Time.now.httpdate, "content-type"
+        header["user-agent"] = "rubysdk/0.0.1"
+        JSON.parse(rest_client.post url, body.to_json, header)
       end
 
       private
       attr_accessor :context, :rest_client, :resource_type
 
-      def header time
+      def header time, content
         signed_auth = context.master_token.generate "get", resource_type, "", time
-        { "accept" => "application/json", "x-ms-version" => context.service_version, "x-ms-date" => time, "authorization" => signed_auth }
+        { content => "application/json", "x-ms-version" => context.service_version, "x-ms-date" => time, "authorization" => signed_auth }
+      end
+
+      def url
+        "#{context.endpoint}/#{resource_type}"
       end
     end
   end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -2,10 +2,9 @@ require 'spec_helper'
 require 'database'
 
 describe Azure::DocumentDB::Database do
-  let(:context) { gimme(Azure::DocumentDB::Context) }
   let(:url) { "our_url" }
-  let(:database1) { {"id" => "ProductDB", "_rid" => "0EwFAA==" , "_ts" => 1408176196, "_self" => "dbs\/0EwFAA==\/", "_etag" => "00001c00-0000-0000-0000-53ef10440000", "_colls" => "colls\/", "_users" => "users\/"} }
-  let(:list_result) { {"_rid"=>"", "Databases" => [database1], "_count" => 1 } }
+  let(:dbs_url) { "#{url}/#{resource_type}" }
+  let(:context) { gimme(Azure::DocumentDB::Context) }
   let(:rest_client) { gimme } # We will inject module RestClient for testability
   let(:master_token) { gimme(Azure::DocumentDB::MasterToken) }
   let(:time) { gimme(Time) }
@@ -13,8 +12,28 @@ describe Azure::DocumentDB::Database do
   let(:signed_auth) { "signed_auth" }
   let(:serv_version) { "2014-08-21" }
   let(:resource_type) { "dbs" }
-  let(:header) { { "accept" => "application/json", "x-ms-version" => serv_version, "x-ms-date" => http_date, "authorization" => signed_auth } }
+  let(:database_name) { "new_database" }
+  let(:accept) { "accept" }
+  let(:content_type) { "content-type" }
+  let(:client) { "rubysdk/0.0.1" }
+
+  let(:list_header) { headers accept }
+  let(:database1) { {"id" => database_name, "_rid" => "0EwFAA==" , "_ts" => 1408176196, "_self" => "dbs\/0EwFAA==\/", "_etag" => "00001c00-0000-0000-0000-53ef10440000", "_colls" => "colls\/", "_users" => "users\/"} }
+  let(:list_result) { {"_rid"=>"", "Databases" => [database1], "_count" => 1 } }
+
+  let(:create_header) {
+    headers = headers content_type
+    headers["user-agent"] = client
+    headers
+  }
+  let(:create_body) { { "id" => database_name } }
+  let(:create_response) { { "id" => "database_name", "_rid" => "K7J6AA==", "_ts" => 1408176280, "_self" => "dbs\/K7J6AA==\/", "_etag" => "00001d00-0000-0000-0000-53ef10980000", "_colls" => "colls\/", "_users" => "users\/" } }
+
   let(:database) { Azure::DocumentDB::Database.new context, rest_client }
+
+  def headers type
+    { type => "application/json", "x-ms-version" => serv_version, "x-ms-date" => http_date, "authorization" => signed_auth }
+  end
 
   before(:each) {
     give(context).master_token { master_token }
@@ -22,11 +41,17 @@ describe Azure::DocumentDB::Database do
     give(context).service_version { serv_version }
     give(Time).now { time }
     give(time).httpdate { http_date }
-    give(master_token).generate("get", resource_type, "", http_date) {signed_auth}
-    give(rest_client).get("#{url}/#{resource_type}", header){ list_result.to_json }
+    give(master_token).generate("get", resource_type, "", http_date) { signed_auth }
+    give(master_token).generate("put", resource_type, "", http_date) { signed_auth }
+    give(rest_client).get(dbs_url, list_header) { list_result.to_json }
+    give(rest_client).post(dbs_url, create_body.to_json, create_header) { create_response.to_json }
   }
 
-  it "Can list the existing databases" do
+  it "can list the existing databases" do
     expect(database.list).to eq list_result
+  end
+
+  it "can create a new database" do
+    expect(database.create database_name).to eq create_response
   end
 end
