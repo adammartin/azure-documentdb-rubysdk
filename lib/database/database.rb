778628c94053446c61d9d23826767b012c823992
adammartin <ebondark.od@gmail.com>
2015-04-18 10:43:30 -0500

    Refactor of the database code for clarity

--- lib/database/database.rb
+++ lib/database/database.rb
@@ -14,24 +14,23 @@ module Azure
       end
 
       def list
-        service_header = header httpdate, "accept", "get"
+        service_header = header httpdate, "get", "accept"
         header = Azure::DocumentDB::Header.new.generate ["x-ms-version"], service_header
         JSON.parse(rest_client.get url, header)
       end
 
       def create database_name
         body = { "id" => database_name }
-        service_header = header httpdate, "Content-Type", "post"
+        service_header = header httpdate, "post", "Content-Type"
         header = Azure::DocumentDB::Header.new.generate ["User-Agent", "x-ms-version"], service_header
         JSON.parse(rest_client.post url, body.to_json, header)
       end
 
       def delete database_id
         delete_url = url database_id
-        time = httpdate
-        signed_auth = context.master_token.generate "delete", resource_type, "", time
-        delete_header = Azure::DocumentDB::Header.new.generate ["User-Agent", "x-ms-version"], { "x-ms-date" => time, "authorization" => signed_auth }
-        rest_client.delete delete_url, delete_header
+        service_header = header httpdate, "delete"
+        header = Azure::DocumentDB::Header.new.generate ["User-Agent", "x-ms-version"], service_header
+        rest_client.delete delete_url, header
       end
 
       private
@@ -41,9 +40,11 @@ module Azure
         Time.now.httpdate
       end
 
-      def header time, content, verb
+      def header time, verb, content = nil
         signed_auth = context.master_token.generate verb, resource_type, "", time
-        { content => "application/json", "x-ms-date" => time, "authorization" => signed_auth }
+        hash = { "x-ms-date" => time, "authorization" => signed_auth }
+        hash[content] = "application/json" if content
+        hash
       end
 
       def url resource_id = nil
