082e4e7edadb6f3b43cc635535f5cac2bbff85a4
adammartin <ebondark.od@gmail.com>
2015-04-18 10:37:30 -0500

    Initial delete functionality not integration tested yet

--- lib/database/database.rb
+++ lib/database/database.rb
@@ -14,28 +14,41 @@ module Azure
       end
 
       def list
-        service_header = header Time.now.httpdate, "accept", "get"
+        service_header = header httpdate, "accept", "get"
         header = Azure::DocumentDB::Header.new.generate ["x-ms-version"], service_header
         JSON.parse(rest_client.get url, header)
       end
 
       def create database_name
         body = { "id" => database_name }
-        service_header = header Time.now.httpdate, "Content-Type", "post"
+        service_header = header httpdate, "Content-Type", "post"
         header = Azure::DocumentDB::Header.new.generate ["User-Agent", "x-ms-version"], service_header
         JSON.parse(rest_client.post url, body.to_json, header)
       end
 
+      def delete database_id
+        delete_url = url database_id
+        time = httpdate
+        signed_auth = context.master_token.generate "delete", resource_type, "", time
+        delete_header = Azure::DocumentDB::Header.new.generate ["User-Agent", "x-ms-version"], { "x-ms-date" => time, "authorization" => signed_auth }
+        rest_client.delete delete_url, delete_header
+      end
+
       private
       attr_accessor :context, :rest_client, :resource_type
+
+      def httpdate
+        Time.now.httpdate
+      end
 
       def header time, content, verb
         signed_auth = context.master_token.generate verb, resource_type, "", time
         { content => "application/json", "x-ms-date" => time, "authorization" => signed_auth }
       end
 
-      def url
-        "#{context.endpoint}/#{resource_type}"
+      def url resource_id = nil
+        target = "/" + resource_id if resource_id
+        "#{context.endpoint}/#{resource_type}#{target}"
       end
     end
   end
--- spec/database/database_spec.rb
+++ spec/database/database_spec.rb
@@ -3,6 +3,7 @@ require 'database/database'
 
 describe Azure::DocumentDB::Database do
   let(:url) { "our_url" }
+  let(:resource_type) { "dbs" }
   let(:dbs_url) { "#{url}/#{resource_type}" }
   let(:context) { gimme(Azure::DocumentDB::Context) }
   let(:rest_client) { gimme } # We will inject module RestClient for testability
@@ -11,7 +12,6 @@ describe Azure::DocumentDB::Database do
   let(:http_date) { "http_date" }
   let(:signed_auth) { "signed_auth" }
   let(:serv_version) { "2014-08-21" }
-  let(:resource_type) { "dbs" }
   let(:database_name) { "new_database" }
   let(:accept) { "accept" }
   let(:content_type) { "Content-Type" }
@@ -30,6 +30,11 @@ describe Azure::DocumentDB::Database do
   let(:create_body) { { "id" => database_name } }
   let(:create_response) { { "id" => database_name, "_rid" => database_id, "_ts" => 1408176280, "_self" => "dbs\/K7J6AA==\/", "_etag" => "00001d00-0000-0000-0000-53ef10980000", "_colls" => "colls\/", "_users" => "users\/" } }
 
+  let(:delete_header) {
+    {"User-Agent" => client, "x-ms-date" => http_date, "x-ms-version" => serv_version, "authorization" => signed_auth}
+  }
+  let(:delete_url) { "#{url}/#{resource_type}/#{database_id}" }
+
   let(:database) { Azure::DocumentDB::Database.new context, rest_client }
 
   def headers type
@@ -44,6 +49,7 @@ describe Azure::DocumentDB::Database do
     give(time).httpdate { http_date }
     give(master_token).generate("get", resource_type, "", http_date) { signed_auth }
     give(master_token).generate("post", resource_type, "", http_date) { signed_auth }
+    give(master_token).generate("delete", resource_type, "", http_date) { signed_auth }
     give(rest_client).get(dbs_url, list_header) { list_result.to_json }
     give(rest_client).post(dbs_url, create_body.to_json, create_header) { create_response.to_json }
   }
@@ -54,5 +60,10 @@ describe Azure::DocumentDB::Database do
 
   it "can create a new database" do
     expect(database.create database_name).to eq create_response
+  end
+
+  it "can delete a supplied database" do
+    database.delete database_id
+    verify(rest_client).delete delete_url, delete_header
   end
 end
