bf0ae564462d1f87b9bf37f4aaa78df5763dbd82
adammartin <ebondark.od@gmail.com>
2015-08-12 16:11:03 -0500

    Commiting the query object that currently is broken :( but gives us a framework to start from

---
+++ lib/query/query.rb
@@ -0,0 +1,30 @@
+require_relative 'query_param'
+require_relative '../context'
+require_relative '../header/secure_header'
+require_relative '../header/custom_query_header'
+require_relative '../auth/master_token'
+
+module Azure
+  module DocumentDB
+    class Query
+      def initialize context, rest_client, resource_type, parent_resource_id, uri
+        self.context = context
+        self.rest_client = rest_client
+        self.resource_type = resource_type
+        self.secure_header = Azure::DocumentDB::SecureHeader.new context.master_token, resource_type
+        self.parent_resource_id = parent_resource_id
+        self.url = uri
+      end
+
+      def execute query, custom_query_header, query_params
+        raw_header = secure_header.header "post", parent_resource_id
+        header = custom_query_header.header raw_header
+        request = { :query => query, :parameters => query_params.params }
+        rest_client.post(url, request, header)
+      end
+
+      private
+      attr_accessor :context, :rest_client, :resource_type, :secure_header, :parent_resource_id, :url
+    end
+  end
+end
---
+++ spec/query/query_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'query/query'
+require 'context'
+require 'header/secure_header'
+require 'header/custom_query_header'
+require 'auth/master_token'
+require 'query/query_param'
+
+describe Azure::DocumentDB::Query do
+  let(:uri) { "full uri of the resource type" }
+  let(:resource_type) { Azure::DocumentDB::ResourceType.DOCUMENT } # Resource type the query will be ran against
+  let(:parent_resource_id) { "parent resource id for example the database '_rid' if you are going to query a collection" }
+  let(:query) { "select * from resource" }
+  let(:raw_secure_header) { "secure_header" }
+  let(:full_header) { "full_header" }
+  let(:params) { "parameters" }
+  let(:query_params) { gimme(Azure::DocumentDB::QueryParameter) }
+  let(:custom_query_header) { gimme(Azure::DocumentDB::CustomQueryHeader) }
+  let(:rest_client) { gimme }
+  let(:context) { gimme(Azure::DocumentDB::Context) }
+  let(:master_token) { gimme(Azure::DocumentDB::MasterToken) }
+  let(:secure_header) { gimme(Azure::DocumentDB::SecureHeader) }
+  let(:response) { gimme }
+  let(:query_body) { { :query => query, :parameters => params } }
+
+  let(:db_query) { Azure::DocumentDB::Query.new context, rest_client, resource_type, parent_resource_id, uri }
+
+  before(:each) {
+    give(context).master_token { master_token }
+    give(custom_query_header).header(raw_secure_header) { full_header }
+    give(Azure::DocumentDB::SecureHeader).new(master_token, resource_type) { secure_header }
+    give(query_params).params { params }
+    give(secure_header).header("post", parent_resource_id) { raw_secure_header }
+    give(rest_client).post(uri, query_body, full_header) { response }
+  }
+
+  it "will return results of the query" do
+    expect(db_query.execute query, custom_query_header, query_params).to eq response
+  end
+end
